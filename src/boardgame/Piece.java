ccfd46dd0b508151c16ac71bb2d57df49b7704f1
Rogers <rogersgbc@gmail.com>
2021-05-01 22:56:15 -0300

    Create abstract method to moves + Change classes to abstract

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
     protected Position position;
     private Board board;
 
@@ -10,5 +10,25 @@ public class Piece {
 
     protected Board getBoard() {
         return board;
+    }
+
+    public abstract boolean[][] possibleMoves();
+
+    public boolean canMove() {
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+    public boolean isThereAnyPossibleMove() {
+        boolean[][] possibleMoves = possibleMoves();
+
+        for (int i = 0; i < possibleMoves.length; i++) {
+            for (int j = 0; j < possibleMoves.length; j++) {
+                if (possibleMoves[i][j]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
