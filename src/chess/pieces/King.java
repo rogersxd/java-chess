d51d1a5c85ca2d1820b030e1b25c54d9605ed895
Rogers <rogersgbc@gmail.com>
2021-05-16 00:07:25 -0300

    Possible moves King

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -17,6 +18,85 @@ public class King extends ChessPiece {
 
     @Override
     public boolean[][] possibleMoves() {
-        return new boolean[0][];
+        boolean[][] matrix = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position positionAux = new Position(0, 0);
+
+        getAboveMoves(matrix, positionAux);
+        getBelowMoves(matrix, positionAux);
+        getLeftMoves(matrix, positionAux);
+        getRightMoves(matrix, positionAux);
+
+        return matrix;
+    }
+
+    private boolean[][] getAboveMoves(boolean[][] matrix, Position positionAux) {
+        positionAux.setValues(position.getRow() - 1, position.getColumn());
+
+        if (getBoard().positionExistsByPosition(positionAux) && hasAvailableMovement(positionAux)) {
+            matrix[positionAux.getRow()][positionAux.getColumn()] = true;
+        }
+
+        positionAux.setValues(position.getRow() - 1, position.getColumn() + 1);
+
+        if (getBoard().positionExistsByPosition(positionAux) && hasAvailableMovement(positionAux)) {
+            matrix[positionAux.getRow()][positionAux.getColumn()] = true;
+        }
+
+        positionAux.setValues(position.getRow() - 1, position.getColumn() - 1);
+
+        if (getBoard().positionExistsByPosition(positionAux) && hasAvailableMovement(positionAux)) {
+            matrix[positionAux.getRow()][positionAux.getColumn()] = true;
+        }
+
+        return matrix;
+    }
+
+    private boolean[][] getBelowMoves(boolean[][] matrix, Position positionAux) {
+        positionAux.setValues(position.getRow() + 1, position.getColumn());
+
+        if (getBoard().positionExistsByPosition(positionAux) && hasAvailableMovement(positionAux)) {
+            matrix[positionAux.getRow()][positionAux.getColumn()] = true;
+        }
+
+        positionAux.setValues(position.getRow() + 1, position.getColumn() + 1);
+
+        if (getBoard().positionExistsByPosition(positionAux) && hasAvailableMovement(positionAux)) {
+            matrix[positionAux.getRow()][positionAux.getColumn()] = true;
+        }
+
+        positionAux.setValues(position.getRow() + 1, position.getColumn() - 1);
+
+        if (getBoard().positionExistsByPosition(positionAux) && hasAvailableMovement(positionAux)) {
+            matrix[positionAux.getRow()][positionAux.getColumn()] = true;
+        }
+
+        return matrix;
+    }
+
+    private boolean[][] getLeftMoves(boolean[][] matrix, Position positionAux) {
+        positionAux.setValues(position.getRow(), position.getColumn() - 1);
+
+        if (getBoard().positionExistsByPosition(positionAux) && hasAvailableMovement(positionAux)) {
+            matrix[positionAux.getRow()][positionAux.getColumn()] = true;
+        }
+
+        return matrix;
+    }
+
+    private boolean[][] getRightMoves(boolean[][] matrix, Position positionAux) {
+        positionAux.setValues(position.getRow(), position.getColumn() + 1);
+
+        if (getBoard().positionExistsByPosition(positionAux) && hasAvailableMovement(positionAux)) {
+            matrix[positionAux.getRow()][positionAux.getColumn()] = true;
+        }
+
+        return matrix;
+    }
+
+    private boolean hasAvailableMovement(Position position) {
+        ChessPiece piece = (ChessPiece) getBoard().piece(position);
+
+        return piece == null || piece.getColor() != getColor();
     }
 }
